d289508242bb1b49d5c2802f61183a41dbbd0d5f
08Uzer <121750611+08Uzair@users.noreply.github.com>
2025-01-01 15:13:31 +0530

    OTP validitation :done

--- src/api/profile/routes/profile.js
+++ src/api/profile/routes/profile.js
@@ -1,9 +1,24 @@
 'use strict';
 
-/**
- * profile router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::profile.profile');
+module.exports = {
+  routes: [
+    {
+      method: 'POST',
+      path: '/send-otp',
+      handler: 'otp.sendOtp',
+      config: {
+        policies: [],
+        description: 'Send OTP to the user\'s email.',
+      },
+    },
+    {
+      method: 'POST',
+      path: '/verify-otp',
+      handler: 'otp.verifyOtp',
+      config: {
+        policies: [],
+        description: 'Verify the OTP sent to the user.',
+      },
+    },
+  ],
+};
